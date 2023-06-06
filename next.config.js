aecfbb7d16d852bcf7d4dec4be32fbf49c91f101
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-06-06 09:50:46 +0800

    refactor: added env on next conf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
-}
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    ENCRYPT_KEY: process.env.ENCRYPT_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
